91e58866521712878214ffbe9a2f8084ba409d22
Humam Afif <Humamafif2002@gmail.com>
2025-05-08 00:19:59 +0700

    menambah page register

--- resources/views/components/pengunjung-layout.blade.php
+++ resources/views/components/pengunjung-layout.blade.php
@@ -6,15 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
-    <main >
+    <main>
         {{ $slot }}
     </main>
     <script src="{{ asset('js/jquery.js') }}"></script>
-
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+        AOS.init();
+    </script>
 </body>
 
 </html>
